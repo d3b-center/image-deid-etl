ded80a14cd027ea8a15a5a09bd3a542c682b16cf
Ariana <arianafamiliar@hotmail.com>
2026-02-05 12:45:19 -0500

    fix syntax errors

--- src/image_deid_etl/image_deid_etl/__main__.py
+++ src/image_deid_etl/image_deid_etl/__main__.py
@@ -230,8 +230,8 @@ def run(args) -> int:
             logger.info("Uploading identified DICOMs to HIPAA-compliant Flywheel instance.")
             retcode = upload2fw(args, nifti_flag=0)
             if retcode != 0:
-                patient_id = glob(local_path + "DICOMs/*")[0].split('/')[-1]
-                accession_number = glob(local_path + "DICOMs/*/*")[0].split('/')[-1].split(' ')[0]
+                patient_id = glob(f"{local_path}/DICOMs/*/")[0].split('/')[-2]
+                accession_number = glob(f"{local_path}/DICOMs/*/*/")[0].split('/')[-2].split(' ')[0]
                 print(f'     Orthanc PatientID: {patient_id}')
                 print(f'     Orthanc AccessionNumber: {accession_number}')
                 raise RuntimeError(
@@ -348,7 +348,7 @@ def upload2fw(args, nifti_flag) -> int:
             source_path = f"{args.program}/{args.site}/DICOMs/"
             fw_project = 'CHOP_raw_data'
             # handle missing PatientID
-            patient_id = glob(f"{source_path}/*")[0].split('/')[-1]
+            patient_id = glob(f"{source_path}/*/")[0].split('/')[-2]
             if patient_id == 'Unknown Patient':
                 subject_label = 'Unknown_Patient'
                 subject_string = f' --subject {subject_label}'
